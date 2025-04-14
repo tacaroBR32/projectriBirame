aabdbf9aff3d48074fecc9dba484c2803e3e7dc3
tacaroBR32 <bryanguilima7912@gmail.com>
2025-04-14 20:48:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+﻿const olka = document.querySelector('.olka');
+const cano= document.querySelector('.cano');
+
+const jump = () =>{
+    olka.classList.add('jump');
+
+
+setTimeout(() => {
+    olka.classList.remove('jump');
+},500);
+}
+   
+
+
+const loop= setInterval(() => {
+    const canoposição = cano.offsetLeft;
+    const olkaposition = +window.getComputedStyle(olka).bottom.replace('px', '');
+
+   
+    if(canoposição < 370 && canoposição > 290  && olkaposition < 70){
+    cano.style.animation = 'none';
+    cano.style.left= `${canoposição}px`;
+
+    olka.style.animation = 'none';
+    olka.style.bottom = `${olkaposition}px`;
+}
+    
+}, 10);
+
+   
+document.addEventListener('keydown', jump);    
